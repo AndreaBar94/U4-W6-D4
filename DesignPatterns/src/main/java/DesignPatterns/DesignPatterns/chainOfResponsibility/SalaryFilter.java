f84ad4107bd27e0135b399655a164cb05cb309bc
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-15 16:45:46 +0200

    added salary filter class

---
+++ DesignPatterns/src/main/java/DesignPatterns/DesignPatterns/chainOfResponsibility/SalaryFilter.java
@@ -0,0 +1,11 @@
+package DesignPatterns.DesignPatterns.chainOfResponsibility;
+
+public class SalaryFilter extends Filter{
+
+	@Override
+	public void check(long salary, Role role) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
